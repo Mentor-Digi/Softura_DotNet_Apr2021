3e0fa880d3e61fc3220f4b9786b8fdb5519a53ff
Gayathri Mahadevan <gayat_19@yahoo.com>
2021-05-12 16:02:52 +0530

    Day20

---
+++ Day20/FirstWebApplication/Controllers/FirstController.cs
@@ -0,0 +1,45 @@
+﻿using FirstWebApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstWebApplication.Controllers
+{
+    public class FirstController : Controller
+    {
+        int[] arr = { 12, 34, 5, 6, 89, 56, 90 };
+        public IActionResult Index()
+        {
+            ViewData["Name"] = "Ramu";
+            ViewData["Age"] = 21;
+            ViewBag.CustomerPhone = "987654321";
+            ViewBag.Price = 123.42;
+            TempData["Post"] = "This is the post that i want to  post";//gfrom one view to another view
+            //Once the tempdata once got will delete
+            var numbers = arr.Where(num => num < 75);
+            ViewData["scores"] = numbers;
+            ViewBag.Scores = numbers;
+            return View();
+        }
+
+        public IActionResult About()
+        { 
+            return View();
+        }
+
+        public IActionResult ShowPost()
+        {
+            Post post = new Post();
+            post.Id = 101;
+            post.PostText = "Check this out. test Post";
+            post.Category = "Text";
+            return View(post);
+        }
+        //public string Index()
+        //{
+        //    return "Hello from conroller";
+        //}
+    }
+}
---
+++ Day20/FirstWebApplication/Controllers/PostController.cs
@@ -0,0 +1,48 @@
+﻿using FirstWebApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstWebApplication.Controllers
+{
+    public class PostController : Controller
+    {
+        static List<Post> posts = new List<Post>()
+        {
+            new Post(){Id=101,PostText="Check the satus of movies on May 14th",Category="Film"},
+            new Post(){Id=102,PostText="Alway wash hands with soap",Category="Health"},
+            new Post(){Id=103,PostText="New arraivals!!! Rose Milk and Badam Milk",Category="Food"}
+        };
+        public IActionResult Index()
+        {
+            return View(posts);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Post post)
+        {
+            posts.Add(post);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            int idx = posts.FindIndex(p => p.Id == id);
+            return View(posts[idx]);
+        }
+        [HttpPost]
+        public IActionResult Edit(int id,Post post)
+        {
+            int idx = posts.FindIndex(p => p.Id == id);
+            posts[idx].PostText = post.PostText;
+            posts[idx].Category = post.Category;
+            return RedirectToAction("Index");
+        }
+    }
+}
